e5700102df758eaa79c7ea1e4c37752ab8309887
Mohamed-Amine Ez-zaoui <77668027+n1motv@users.noreply.github.com>
2025-02-10 13:53:57 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-echo "🔧 Installing Tesseract and Poppler for Render..."
+echo "🔧 Installing Poppler for Render..."
 
 # Créer un dossier pour les binaires
-mkdir -p /opt/tesseract
 mkdir -p /opt/poppler
-
-# Télécharger et installer Tesseract (binaire portable)
-curl -L -o /opt/tesseract/tesseract https://github.com/tesseract-ocr/tesseract/releases/download/5.3.3/tesseract-5.3.3-linux-x86_64
-chmod +x /opt/tesseract/tesseract
-
 # Télécharger et installer Poppler (binaire portable)
 curl -L -o /opt/poppler/pdftoppm https://poppler.freedesktop.org/poppler-24.02.0.tar.xz
 chmod +x /opt/poppler/pdftoppm
